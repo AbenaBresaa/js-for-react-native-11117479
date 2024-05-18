bb71ea02d1481168b4915377d1853e9201f47382
Abena Kyerewaa Bresaa <abenakyerewaa.06@gmail.com>
2024-05-18 20:45:38 +0000

    Second task

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -11,3 +11,21 @@ console.log(processedNumbers);
 //output: [33,144,39,196,45]
 
 module.export = {processArray};
+
+
+
+//task 2
+function formatArrayStrings(strings,numbers){
+  return strings.map((str,index) => {
+    const num = numbers[index];
+    return num % 2 === 0 ? str . toUpperCase() : str . LowerCase()
+  });
+}
+
+//example 
+const strings = [Kofi,Abena,Jhay,Kwame,Ama];
+const formattedStrings = formatArrayStrings(strings,processedNumbers);
+console.log (formattedStrings);
+//output: [KOFI,Abena,JHAY,Kwame,Ama]
+
+module.exports = {processArray,formatArrayStrings};
